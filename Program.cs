73f7a02b3da8a94ac19d22bf34a1a6f58c47026e
HP <HP@ABIR-MOHAMMAD>
2021-03-14 23:41:08 +0600

    Code Committed

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Association
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department d1 = new Department("Computer Science and Engineering", "CSE");
+            Student S1 = new Student("Iffath Jahan Jothi", "110011", 3.50f);
+            S1.showDetails();
+        }
+    }
+}
+
